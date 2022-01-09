5167a2b39d352ec1a9b984a1f7bbdd4577cdead0
Sion <sion007005@gmail.com>
2022-01-10 05:18:27 +0900

    Initial commit

---
+++ src/main/java/com/sion/jwtlogintest/JwtLoginTestApplication.java
@@ -0,0 +1,13 @@
+package com.sion.jwtlogintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtLoginTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sion/jwtlogintest/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sion.jwtlogintest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello, this is our main entrance.";
+    }
+}
